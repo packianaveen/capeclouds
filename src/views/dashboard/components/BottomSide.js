08ef9605d45c25325f318e1885153047c1a8866a
naveen <naveen@qsime.com>
2023-09-19 23:12:56 +0530

    live push

--- src/views/dashboard/components/BottomSide.js
+++ src/views/dashboard/components/BottomSide.js
@@ -123,7 +123,7 @@ const BottomSide = () => {
             console.log(data);
             // setInitialAd(data);
             axios
-              .post(`${url}/api/bottomadd1`, ad, {
+              .post(`${url}/api/add1`, ad, {
                 headers: {
                   'Content-Type': 'multipart/form-data',
                 },
@@ -156,7 +156,7 @@ const BottomSide = () => {
       };
       console.log(ad.photo);
       axios
-        .post(`${url}/api/bottomadd1`, ad, {
+        .post(`${url}/api/add1`, ad, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
