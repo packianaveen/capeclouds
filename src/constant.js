0cf91c653c317512fe36b930f2de533cfbf19d79
naveen <naveen@qsime.com>
2023-09-30 17:01:44 +0530

    live push

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,8 @@
 var currentLocation = window.location;
 
 export const url =
-  currentLocation.host == 'localhost:3000' ? 'http://localhost:7098' : 'http://192.168.18.71:7098';
+  currentLocation.host == 'localhost:3000'
+    ? 'http://localhost:7098'
+    : 'https://capecloudsbackend.onrender.com';
 export const localurl =
   currentLocation.host == 'localhost:3000' ? 'http://localhost:3000' : 'https://capeclouds.com';
