91f7f63e82d7d750b17d90c256758e5647d56765
naveen <naveen@qsime.com>
2023-10-13 07:16:20 +0530

    live push

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,8 @@
 var currentLocation = window.location;
 
 export const url =
-  currentLocation.host == 'localhost:3000' ? 'http://localhost:7098' : 'https://main--teal-cobbler-35a2bb.netlify.app';
+  currentLocation.host == 'localhost:3000'
+    ? 'http://localhost:7098'
+    : 'https://backend-five-nu.vercel.app/';
 export const localurl =
   currentLocation.host == 'localhost:3000' ? 'http://localhost:3000' : 'https://capeclouds.com';
